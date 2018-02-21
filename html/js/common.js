b68030d82125f65aee7c096cae12d2f96e9f524d
anwebdevelopers <anwebdev78@gmail.com>
2018-02-21 18:51:56 +0300

    21.02.2017

--- html/js/common.js
+++ html/js/common.js
@@ -89,4 +89,20 @@ $(function() {
        smartSpeed: 600
    });
 
+   /*******************************************************/
+   //POPUP
+   /*******************************************************/
+
+   $('.popup-with-move-anim').magnificPopup({
+       type: 'inline',
+       fixedContentPos: false,
+       fixedBgPos: true,
+       overflowY: 'auto',
+       closeBtnInside: true,
+       preloader: false,
+       midClick: true,
+       removalDelay: 300,
+       mainClass: 'my-mfp-slide-bottom'
+   });
+
 });
